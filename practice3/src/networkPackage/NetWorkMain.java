e6f02c162238e727493fa5ae00064059fb3e6825
jw969 <jw969@DESKTOP-SF4QPRO>
2020-06-30 14:05:58 +0900

    네트워크 테스트

--- practice3/src/networkPackage/NetWorkMain.java
+++ practice3/src/networkPackage/NetWorkMain.java
@@ -1,9 +1,11 @@
 package networkPackage;
 
 public class NetWorkMain {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) {	
+		
 		System.out.printf("dddd");
+		System.out.printf("dddd");
+		
 	}
-
 }
