f164aa226d3cd02b01a8fbd370638e57147b0f5d
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-01 12:58:20 +0900

    메인

--- practice3/src/gamePackage/GameMain.java
+++ practice3/src/gamePackage/GameMain.java
@@ -1,11 +1,21 @@
 package gamePackage;
 
+import java.lang.*;
+
 public class GameMain {
 
 	public static void main(String[] args) {
-		long mGUID = 3432424;
-		Character jw = new Character("jaewan1",mGUID);
+
 		
+		for(int i=0;i<20;i++) {
+			CharacterManager.makeGUID();
+		}
+		
+		
+		CharacterManager.makeGUID();
+		
+
+
 	}
 
 }
