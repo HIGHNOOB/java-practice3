4cbcad646163f9a39902e8c0fc1ea79fe1d4d3e0
jw969 <jw969@DESKTOP-SF4QPRO>
2020-07-01 15:14:19 +0900

    network

--- practice3/src/networkPackage/NetWorkMain.java
+++ practice3/src/networkPackage/NetWorkMain.java
@@ -10,6 +10,7 @@ public class NetWorkMain {
 		System.out.printf("NetWork Practice1\n");
         InetAddress local = InetAddress.getLocalHost();//변수 local을 InetAddress.getLocalHost();로 초기화 합니다.
         System.out.println(local.getHostAddress());//local.getHostAddress()로 현재 자신의 컴퓨터의 ip를 알 수 있습니다
+        
+        
     }
 }
-
