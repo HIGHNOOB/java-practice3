1448c69eae4d3ba6401d1f31b6c5d17fa4d70a17
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-01 12:58:31 +0900

    아이템정의

---
+++ practice3/src/gamePackage/ItemDef.java
@@ -0,0 +1,16 @@
+package gamePackage;
+
+public class ItemDef {
+	public static int LONG_SWORD = 101;
+	public static int BF_SWORD = 102;
+	public static int DORANS_RING = 103;
+	public static int DORANS_BLADE = 104;
+	
+	public static int DORANS_SHIELD = 201;
+	public static int CLOTH_ARMOR = 202;
+	
+	public static int HEALTH_POTION = 301;
+	public static int MANA_POTION = 302;
+	public static int VISION_WARD = 303;
+
+}
