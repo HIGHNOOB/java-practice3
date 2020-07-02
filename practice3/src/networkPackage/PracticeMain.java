3b9c28859f6cf5a993c3550d00562bbb93103a8b
jw969 <jw969@DESKTOP-SF4QPRO>
2020-07-02 15:34:07 +0900

    지울거임

---
+++ practice3/src/networkPackage/PracticeMain.java
@@ -0,0 +1,18 @@
+package networkPackage;
+
+import java.util.HashMap;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		
+		/*
+		final HashMap<Long,String> hmap = new HashMap<Long, String>;
+		
+		hmap.put(100, "น้");
+		*/
+		
+
+	}
+
+}
