a86c6400d8f8355d8ce4ceb4768cfd593e7edeb0
jw969 <jw969@DESKTOP-SF4QPRO>
2020-07-02 16:34:35 +0900

    아이템code와 아이템name이 같으니 hashmap으로 통합해볼까? (시도중 or ItemManager클래스에서 처리할듯)

--- practice3/src/gamePackage/ItemDef.java
+++ practice3/src/gamePackage/ItemDef.java
@@ -1,5 +1,7 @@
 package gamePackage;
 
+import java.util.HashMap;
+
 public class ItemDef {
 	public static final int LONG_SWORD = 1001;
 	public static final int BF_SWORD = 1002;
@@ -15,4 +17,15 @@ public class ItemDef {
 	public static final int MANA_POTION = 3002;
 	public static final int VISION_WARD = 3003;
 
+	
+	/* TODO 아이템 클래스에서
+	 * 아이템코드와 아이템이름은 어차피 같은 것이다 이것을 통합하는것이 좋을것같음
+	 *그것을 통합하기 위한 방식으로 hashMap을 사용해 보려고 하였으나 일단보류
+	 *or itemManager에서 처리할 수 있을듯
+	HashMap<Integer, String> mItemMap = new HashMap<Integer, String>();
+	
+	void initItem() {
+		mItemMap.put(LONG_SWORD, "long");
+	}
+	*/
 }
