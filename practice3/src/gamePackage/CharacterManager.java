a839fe17676d1a96a6dd0714b3d19578bccbc156
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-02 12:55:02 +0900

    add final

--- practice3/src/gamePackage/CharacterManager.java
+++ practice3/src/gamePackage/CharacterManager.java
@@ -1,28 +1,35 @@
 package gamePackage;
 
+import java.util.HashMap;
+
 public class CharacterManager {
-	
-	public boolean createCharacter(String name) {
-		if(isNameDuplicated(name)) {
-			System.out.printf("중복된 이름입니다.");
-			return false;
+
+	private static HashMap<Long, String> mCharacterNames;
+
+	public static Character createCharacter(String name) {
+		if (isNameDuplicated(name)) {
+			System.out.printf("[캐릭터매니저] 생성실패");
+			return null;
 		}
-		Character playerCharacter = new Character(name, makeGUID());
-		System.out.printf("%s:생성완료", name);
-		return true;
+
+		Object nameObj = mCharacterNames.get(name);
+		
+		long guid =GUIDmanager.makeGUID(GUIDmanager.GUID_CHARACTER);
+		System.out.printf("[CharacterManager] %s:생성완료%n", name);
+		
+		
+		Character playerCharacter =new Character(name, guid);
+
+		mCharacterNames.put(guid,name);
+		return new Character(name,guid);
 	}
-	
-	public boolean isNameDuplicated(String name) {
-		//TODO 닉네임 중복확인
-		return true;
-	}
-	
-	public static long makeGUID() {
-		//TODO 지금방식은 동시에 생성시 GUID가 겹칠 수 있는 문제가 있음
-		long GUID = System.currentTimeMillis()%1000000;
-		GUID = (long)(GUID*Math.random());
-		System.out.printf("GUID: %d 생성%n", GUID);
-		return GUID;
+
+	public static boolean isNameDuplicated(String name) {
+		if(mCharacterNames.containsValue(name)) {
+			System.out.printf("[CharacterManager] %s: 중복된 이름입니다.%n", name);
+			return true;
+		}
+		return false;
 	}
 
 }
--- practice3/src/gamePackage/ItemDef.java
+++ practice3/src/gamePackage/ItemDef.java
@@ -1,16 +1,18 @@
 package gamePackage;
 
 public class ItemDef {
-	public static int LONG_SWORD = 101;
-	public static int BF_SWORD = 102;
-	public static int DORANS_RING = 103;
-	public static int DORANS_BLADE = 104;
+	public static final int LONG_SWORD = 1001;
+	public static final int BF_SWORD = 1002;
+	public static final int DORANS_RING = 1003;
+	public static final int DORANS_BLADE = 1004;
 	
-	public static int DORANS_SHIELD = 201;
-	public static int CLOTH_ARMOR = 202;
+	public static final int DORANS_SHIELD = 2001;
+	public static final int CLOTH_ARMOR = 2002;
+	public static final int MAGICIAN_HAT = 2003;
+	public static final int LEATHER_SHOSE = 2004;
 	
-	public static int HEALTH_POTION = 301;
-	public static int MANA_POTION = 302;
-	public static int VISION_WARD = 303;
+	public static final int HEALTH_POTION = 3001;
+	public static final int MANA_POTION = 3002;
+	public static final int VISION_WARD = 3003;
 
 }
