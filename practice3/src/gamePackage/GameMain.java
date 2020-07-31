57adbcd97fbf118374e30b4a496e34acae504fc8
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-31 12:50:39 +0900

    [skills] ..

--- practice3/src/gamePackage/GameMain.java
+++ practice3/src/gamePackage/GameMain.java
@@ -6,11 +6,11 @@ public class GameMain {
 
 	public static void main(String[] args) {
 		
-		Character jw =	CharacterManager.createCharacter("Àç¿Ï");
-		Character jw2 =	CharacterManager.createCharacter("wodhks");
-		Character jw3 =	CharacterManager.createCharacter("wodhks");//name duplicated
-		Character jw4 =	CharacterManager.createCharacter("wodhks2");
-		Character jw5 =	CharacterManager.createCharacter("wodhks3");
+		Character jw =	CharacterManager.createCharacter("wodhks");
+		Character jw2 =	CharacterManager.createCharacter("wodhks2");
+		Character jw3 =	CharacterManager.createCharacter("wodhks2");//name duplicated
+		Character jw4 =	CharacterManager.createCharacter("wodhks3");
+		Character jw5 =	CharacterManager.createCharacter("wodhks4");
 		
 		jw.showStats();
 		jw.showSkills();
