4c7606a46e943d2c90d8a0349aa48db65bb252d6
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-31 12:16:12 +0900

    .

--- practice3/src/gamePackage/CharacterManager.java
+++ practice3/src/gamePackage/CharacterManager.java
@@ -11,25 +11,36 @@ public class CharacterManager {
 			System.out.printf("[CharacterManager] 생성실패%n");
 			return null;
 		}
-		
-		long guid =GUIDmanager.makeGUID(GUIDmanager.GUID_CHARACTER);
-		mCharacterNames.put(guid,name);
-		
-		Character character = new Character(name,guid);
-		
+
+		long guid = GUIDmanager.makeGUID(GUIDmanager.GUID_CHARACTER);
+		mCharacterNames.put(guid, name);
+
+		Character character = new Character(name, guid);
+		System.out.printf("[CharacterManager] >%s< 생성완료%n", character.getName());
+
+		initCharacterItems(character);
+		System.out.printf("[CharacterManager] >%s< 기본 아이템 지급 완료%n", character.getName());
+
 		return character;
 	}
 
 	public static boolean isNameDuplicated(String name) {
-		if(mCharacterNames.containsValue(name)) {
+		if (mCharacterNames.containsValue(name)) {
 			System.out.printf("[CharacterManager] %s: 중복된 이름입니다.%n", name);
 			return true;
 		}
-		return false; 
+		return false;
 	}
+
 	boolean giveItem(Character character, int itemCode, int count) {
 		character.addItem(itemCode, count);
 		return true;
 	}
 
+	public static void initCharacterItems(Character character) {
+		character.addItem(ItemDef.DORANS_BLADE, 1);
+		character.addItem(ItemDef.CLOTH_ARMOR, 1);
+		character.addItem(ItemDef.LEATHER_SHOSE, 1);
+		character.addItem(ItemDef.HEALTH_POTION, 3);
+	}
 }
