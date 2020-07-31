e60bf174ce12826131be28954afa270a31986023
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-07-31 12:16:01 +0900

    .

--- practice3/src/gamePackage/GameMain.java
+++ practice3/src/gamePackage/GameMain.java
@@ -13,9 +13,8 @@ public class GameMain {
 		//System.out.printf("guid: %d%n", jw.getGUID());
 		//System.out.printf("guid: %d%n", jw2.getGUID()); //name duplicated
 		
-		System.out.printf("name: %s%n", jw3.getName());
-		System.out.printf("level: %d%n", jw3.getLevel());
-		System.out.printf("GUID: %d%n", jw3.getGUID());	
+		jw.showStats();
+		jw.showItems();
 		
 	} 
 }
